c6a8337355f4680080b14069602e0740ab06a05c
brasilfarn-commits <brasilfarn@users.noreply.github.com>
2026-07-12 00:56:12 -0300

    Fix SW: remove caching, network only

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,10 @@
-const CACHE_NAME = 'farn-v2';
-const ASSETS = [
-    './',
-    './index.html',
-    './style.css',
-    './app.js',
-    './manifest.json',
-    './logo-farn.png.png'
-];
+const CACHE_NAME = 'farn-v3';
 
 self.addEventListener('install', e => {
     e.waitUntil(
         caches.keys().then(keys => Promise.all(
             keys.map(k => caches.delete(k))
-        )).then(() => caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)))
+        ))
     );
     self.skipWaiting();
 });
@@ -20,7 +12,7 @@ self.addEventListener('install', e => {
 self.addEventListener('activate', e => {
     e.waitUntil(
         caches.keys().then(keys => Promise.all(
-            keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+            keys.map(k => caches.delete(k))
         ))
     );
     self.clients.claim();
@@ -28,12 +20,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
     e.respondWith(
-        fetch(e.request).then(res => {
-            if (res && res.status === 200) {
-                const clone = res.clone();
-                caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
-            }
-            return res;
-        }).catch(() => caches.match(e.request))
+        fetch(e.request).catch(() => caches.match(e.request))
     );
 });
